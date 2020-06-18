8733348a9d82c63226a06cea2adad27a60cd23b1
WissamAntoun <wissam.antount@gmail.com>
2020-06-18 23:21:44 +0300

    .

---
+++ tabnet.py
@@ -0,0 +1,140 @@
+#%%
+from pytorch_tabnet import tab_model
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+#%%
+df = pd.read_csv("data/train.csv")
+df = df.fillna(-1)
+#%%
+target = 'y'
+Non_data_col = ['galaxy','y','galactic year']
+#Choose all predictors except target & IDcols
+predictors = [x for x in df.columns if x not in Non_data_col]
+#%%
+df2 = df[predictors]
+
+x_train , x_test, y_train, y_test = train_test_split(df2,df[target],test_size=0.2,random_state=42)
+
+print(x_train)
+print(y_train)
+
+print(x_test)
+print(y_test)
+
+np_x_train = np.array(x_train)
+np_y_train = np.array(y_train)
+np_x_test = np.array(x_test)
+np_y_test = np.array(y_test)
+
+np_x_train = np_x_train
+np_y_train = np_y_train.reshape(-1,1) 
+np_x_test = np_x_test 
+np_y_test = np_y_test.reshape(-1,1)
+
+print(np_x_train.shape)
+print(np_y_train.shape)
+print(np_x_test.shape)
+print(np_y_test.shape)
+#%%
+reg = TabNetRegressor(n_d=8, n_a=8, n_steps=3, gamma=1.3,
+                 n_independent=2, n_shared=2, epsilon=1e-15,  momentum=0.02,
+                 lambda_sparse=1e-3, seed=0, lr=2e-2,verbose=1)
+reg.fit(np_x_train, np_y_train , np_x_test, np_y_test)
+
+preds = reg.predict(np_x_test)
+print(mean_squared_error(np_y_test,preds,squared=False))
+
+from sklearn.base import RegressorMixin
+
+X_valid,y_valid = np_x_test, np_y_test
+class sklearntabnetregressor(RegressorMixin):
+  def __init__(self,n_d=8, n_steps=3, gamma=1.3,
+                 n_independent=2, n_shared=2,  momentum=0.02,
+                 lambda_sparse=1e-3, seed=2, lr=2e-2):
+    self.n_d = n_d
+    self.n_steps = n_steps
+    self.gamma = gamma
+    self.n_independent = n_independent
+    self.n_shared = n_shared
+    self.momentum = momentum
+    self.lambda_sparse = lambda_sparse
+    self.lr = lr
+    self.seed = seed
+    self.tabnet = TabNetRegressor(n_d = self.n_d, n_a = self.n_d, n_steps = self.n_steps, gamma = self.gamma,
+                 n_independent = self.n_independent, n_shared = self.n_shared,  momentum = self.momentum,
+                 lambda_sparse = self.lambda_sparse, seed = self.seed, lr = self.lr,verbose=False)
+
+  def fit(self, X, y):
+    self.tabnet.fit(X, y, X_valid,y_valid)
+    return self
+  
+  def predict(self, X):    
+    return self.tabnet.predict(X)
+
+  def get_params(self, deep=True):
+    return {
+        "n_d" : self.n_d, 
+        "n_steps" : self.n_steps, 
+        "gamma" : self.gamma,
+        "n_independent" : self.n_independent, 
+        "n_shared" : self.n_shared, 
+        "momentum" : self.momentum, 
+        "lambda_sparse" : self.lambda_sparse, 
+        "lr" : self.lr, 
+        "seed" : self.seed
+    }
+
+  def set_params(self, **parameters):
+      for parameter, value in parameters.items():
+          setattr(self, parameter, value)
+      return self
+
+from sklearn.model_selection import GridSearchCV
+
+preds = sk.predict(np_x_test)
+print(mean_squared_error(np_y_test,preds,squared=False))
+
+parameters = {
+    "n_d" : range(8,65,16), 
+    "n_steps" : range(3,11,2), 
+    "gamma" : [i/10.0 for i in range(10,21,3)],
+    "n_independent" : range(1,6,2), 
+    "n_shared" : range(1,6,2), 
+    "momentum" : [0.02,0.2], 
+    "lambda_sparse" : [1e-3], 
+    "lr" : [0.02],
+    "seed" : [0]
+}
+
+parameters_test = {
+    "n_d" : range(8,65,300), 
+    "n_steps" : range(3,11,300), 
+    "gamma" : [i/10.0 for i in range(10,21,300)],
+    "n_independent" : range(1,6,200), 
+    "n_shared" : range(1,6,200), 
+    "momentum" : [0.02,0.2], 
+    "lambda_sparse" : [1e-3], 
+    "lr" : [0.02],
+    "seed" : [0]
+}
+
+tabnet = sklearntabnetregressor()
+tabnetgrid = GridSearchCV(tabnet,parameters,scoring='neg_root_mean_squared_error',cv=5,verbose=True,n_jobs=-1)
+
+tabnetgrid.fit(np_x_train, np_y_train)
+
+print(tabnetgrid.best_estimator_)
+print(tabnetgrid.best_params_)
+print(tabnetgrid.best_score_)
+
+
+
+total=1
+for k ,v in parameters.items():
+  print(k,len(v))
+  total*=len(v)
+
+print(total)
+
--- xgbgridsearch.py
+++ xgbgridsearch.py
@@ -9,22 +9,51 @@ import seaborn as sns
 
 import xgboost as xgb
 from xgboost.sklearn import XGBRegressor
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
+from sklearn.ensemble import RandomForestRegressor
 #%%
 from matplotlib.pylab import rcParams
 rcParams['figure.figsize'] = 20, 4
 #%%
 train_df = pd.read_csv("data/train.csv")
 test_df = pd.read_csv("data/test.csv")
+labels = train_df['y']
+
+# train_df = train_df.fillna(0)
+# test_df = test_df.fillna(0)
+
+train_df = train_df.drop('y',axis =1 )
+#%%
+
+# Non_data_col = ['galactic year','y']
+# #%%
+# #Choose all predictors except target & IDcols
+# predictors = [x for x in train_df.columns if x not in Non_data_col]
+#%%
+all_data = pd.concat([train_df,test_df],axis=0,ignore_index=True)
+#%%
+all_data["galaxy"] = all_data["galaxy"].astype('category')
+all_data["galaxy"] = all_data["galaxy"].cat.codes
 
 #%%
-target = 'y'
-Non_data_col = ['galaxy','y']
+
+all_data_without_year_name = all_data.drop(['galactic year','galaxy'],axis=1)
 #%%
-#Choose all predictors except target & IDcols
-predictors = [x for x in train_df.columns if x not in Non_data_col]
+scaler = RobustScaler().fit(all_data_without_year_name)
+all_data_without_year_name_scaled = scaler.transform(all_data_without_year_name)
+#%%
+year_name = all_data[['galactic year','galaxy']]
+all_data_without_year_name_scaled_df = pd.DataFrame(all_data_without_year_name_scaled,columns=all_data_without_year_name.columns)
+#%%
+all_data_scaled = pd.concat([year_name,all_data_without_year_name_scaled_df],axis=1,sort=False)
+#%%
+all_data_scaled = all_data_scaled.fillna(0) 
+#%%
+X_train = all_data_scaled[0:len(train_df)]
+X_test = all_data_scaled[len(train_df):]
 #%%
 xgb1 = XGBRegressor()
-#%%
+
 lr = [i/100.0 for i in range(1,10,2)]
 l2 = [i/10.0 for i in range(1,10,2)]
 lr.extend(l2)
@@ -33,15 +62,15 @@ lr.extend([1,2])
 parameters = { 
                 'objective':['reg:linear'],
                 'learning_rate': lr, 
-                'max_depth': range(10,80,10),
-                'min_child_weight': range(1,7,2),
+                'max_depth': range(5,16,5),
+                'min_child_weight': range(1,5,1),
                 'silent': [1],
                 'subsample': [0.5,0.55,0.6],
-                'colsample_bytree': [0.7,0.8,0.85],
-                'n_estimators': [10,50,100,200,500],
+                'colsample_bytree': [0.65,0.7,0.75],
+                'n_estimators': [200,250,300],
                 'objective':['reg:squarederror'],
-                # 'tree_method':['gpu_hist'],
-                # 'gpu_id':[0]
+                #'tree_method':['gpu_exact'],
+                # 'gpu_id':[0],
                 'seed' : [42]
                 }
 
@@ -49,12 +78,44 @@ xgb_grid = GridSearchCV(xgb1,
                         parameters,
                         scoring='neg_root_mean_squared_error', #this line can be commented to use XGB's default metric
                         cv = 5,
-                        n_jobs = 4,
+                        n_jobs = 8,
                         verbose=True)
 
 
 #%%
-xgb_grid.fit(train_df[predictors],train_df[target])
+xgb_grid.fit(X_train,labels)
 print(xgb_grid.best_estimator_)
 print(xgb_grid.best_score_)
 # %%
+# XGBRegressor(base_score=0.5, booster=None, colsample_bylevel=1,
+#              colsample_bynode=1, colsample_bytree=0.7, gamma=0, gpu_id=-1,
+#              importance_type='gain', interaction_constraints=None,
+#              learning_rate=0.05, max_delta_step=0, max_depth=10,
+#              min_child_weight=3, missing=nan, monotone_constraints=None,
+#              n_estimators=200, n_jobs=0, num_parallel_tree=1, random_state=42,
+#              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=42, silent=1,
+#              subsample=0.55, tree_method=None, validate_parameters=False,
+#              verbosity=None)
+# -0.02789310275675531
+
+# GBRegressor(base_score=0.5, booster=None, colsample_bylevel=1,
+#              colsample_bynode=1, colsample_bytree=0.75, gamma=0, gpu_id=-1,
+#              importance_type='gain', interaction_constraints=None,
+#              learning_rate=0.05, max_delta_step=0, max_depth=10,
+#              min_child_weight=3, missing=nan, monotone_constraints=None,
+#              n_estimators=300, n_jobs=0, num_parallel_tree=1, random_state=42,
+#              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=42, silent=1,
+#              subsample=0.6, tree_method=None, validate_parameters=False,
+#              verbosity=None)
+# -0.022127200450818837
+
+# XGBRegressor(base_score=0.5, booster=None, colsample_bylevel=1,
+#              colsample_bynode=1, colsample_bytree=0.7, gamma=0, gpu_id=-1,
+#              importance_type='gain', interaction_constraints=None,
+#              learning_rate=0.05, max_delta_step=0, max_depth=10,
+#              min_child_weight=4, missing=nan, monotone_constraints=None,
+#              n_estimators=300, n_jobs=0, num_parallel_tree=1, random_state=42,
+#              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=42, silent=1,
+#              subsample=0.6, tree_method=None, validate_parameters=False,
+#              verbosity=None)
+# -0.021227692090664572
